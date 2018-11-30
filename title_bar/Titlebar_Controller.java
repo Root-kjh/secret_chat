d2b56f5dae824f1d70bae8e4ea3665c37a13646e
Drk <wldnro3141@gmail.com>
2018-11-30 17:04:15 +0900

    오류수정

--- title_bar/Titlebar_Controller.java
+++ title_bar/Titlebar_Controller.java
@@ -1,5 +1,9 @@
 package title_bar;
 
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.PointerInfo;
+
 import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
@@ -7,23 +11,31 @@ import javafx.scene.Node;
 import javafx.scene.control.Button;
 
 public class Titlebar_Controller {
-	double x, y;
+	double x, y, scenex,sceney;
 	@FXML
 	private Button min, max, close;
-
+	
 	@FXML
 	void dragged(MouseEvent event) {
 		Stage sta = (Stage) ((Node) event.getSource()).getScene().getWindow();
-		sta.setX(event.getSceneX()-x);
-		sta.setY(event.getSceneY()-y);
+		com.sun.glass.ui.Robot robot = com.sun.glass.ui.Application.GetApplication().createRobot();
+		
+		sta.setX(robot.getMouseX()-(x-scenex)+350);
+		sta.setY(robot.getMouseY()-(y-sceney)+20);
 	}
 
 	@FXML
 	void pressed(MouseEvent event) {
-		x = event.getSceneX();
-		y = event.getSceneY();
-		System.out.println(x);
-		System.out.println(y);
+		com.sun.glass.ui.Robot robot = com.sun.glass.ui.Application.GetApplication().createRobot();
+		Stage sta = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		scenex= event.getSceneX();
+		sceney= event.getSceneY();
+		x=robot.getMouseX();
+		y=robot.getMouseY();
+		System.out.println("scenex : "+scenex);
+		System.out.println("sceney : "+sceney);
+		System.out.println("x: "+x);
+		System.out.println("y : "+y);
 	}
 
 	@FXML
