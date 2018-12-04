3e0b2dee332efc69f6baeb0cd33211ae22826b24
dua1dn1 <45251056+dua1dn1@users.noreply.github.com>
2018-12-04 11:40:21 +0900

    임시테스트2
    임시테스트2

--- title_bar/Titlebar_Controller.java
+++ title_bar/Titlebar_Controller.java
@@ -1,41 +1,32 @@
 package title_bar;
 
-import java.awt.MouseInfo;
-import java.awt.Point;
-import java.awt.PointerInfo;
-
 import javafx.fxml.FXML;
-import javafx.scene.input.MouseEvent;
-import javafx.stage.Stage;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 public class Titlebar_Controller {
-	double x, y, scenex,sceney;
+	double x, y, scenex, sceney;
 	@FXML
 	private Button min, max, close;
 	
+
 	@FXML
 	void dragged(MouseEvent event) {
 		Stage sta = (Stage) ((Node) event.getSource()).getScene().getWindow();
 		com.sun.glass.ui.Robot robot = com.sun.glass.ui.Application.GetApplication().createRobot();
-		
-		sta.setX(robot.getMouseX()-(x-scenex)+350);
-		sta.setY(robot.getMouseY()-(y-sceney)+20);
+
+		sta.setX(robot.getMouseX()- scenex);
+		sta.setY(robot.getMouseY()- sceney);
 	}
 
 	@FXML
-	void pressed(MouseEvent event) {
+	void pressed(MouseEvent event) {	
 		com.sun.glass.ui.Robot robot = com.sun.glass.ui.Application.GetApplication().createRobot();
 		Stage sta = (Stage) ((Node) event.getSource()).getScene().getWindow();
-		scenex= event.getSceneX();
-		sceney= event.getSceneY();
-		x=robot.getMouseX();
-		y=robot.getMouseY();
-		System.out.println("scenex : "+scenex);
-		System.out.println("sceney : "+sceney);
-		System.out.println("x: "+x);
-		System.out.println("y : "+y);
+		scenex = event.getSceneX();
+		sceney = event.getSceneY();
 	}
 
 	@FXML
@@ -54,6 +45,7 @@ public class Titlebar_Controller {
 	void max(MouseEvent event) {
 		Stage sta = (Stage) ((Node) event.getSource()).getScene().getWindow();
 		sta.setFullScreenExitHint("Secret_chat is Full Screen Mode!!!");
+		
 		sta.setFullScreen(true);
 	}
 }
